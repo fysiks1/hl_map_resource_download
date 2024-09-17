bb0514a27a8ce795be873b2568dd53b0f4a91e48
Fysiks <fysiks1@gmail.com>
2024-09-17 00:24:09 -0500

    Major Work
    - Refactoring e.g. code reuse
    - Add map info file
    - Add map detailed texture file
    - Add custom objective icons

--- MapArchiveBuilder.py
+++ MapArchiveBuilder.py
@@ -10,6 +10,7 @@ class MapArchiveBuilder:
 		self.mapname = mapname
 		self.archivePath = os.path.join(outDir, mapname)
 		self.resgenRootPath = resgenRootPath
+		self.resFile = "%s/maps/%s.res" % (self.archivePath, self.mapname)
 
 		if os.path.exists(basePath):
 			self.getter = LocalGetter(basePath, self.archivePath)
@@ -34,33 +35,68 @@ class MapArchiveBuilder:
 			resgenSubprocessArgs.insert(0, "wine")
 		subprocess.run(resgenSubprocessArgs)
 
-		f = open("%s/maps/%s.res" % (self.archivePath, self.mapname), "r")
+		f = open(self.resFile, "r")
 		lines = f.readlines()
 
-		resources = []
+		self.resources = []
 		for line in lines:
 			line = line.strip()
 			if( line and line[0] != "/"):
-				resources.append(line)
+				self.resources.append(line)
 
 		## Download required resources based on the res file
-		for res in resources:
+		for res in self.resources:
 			self.getter.getRelativeFile(res)
 
-		## Look for overview file
+		## Map Info
+		mapDataFile = "maps/%s.txt" % self.mapname
 		try:
-			self.getter.getRelativeFile("overviews/%s.bmp" % self.mapname)
-			with open("%s/maps/%s.res" % (self.archivePath, self.mapname), "a") as myfile:
-				myfile.write("\n\n")
-				myfile.write("overviews/%s.bmp" % self.mapname)
+			self.getter.getRelativeFile(mapDataFile)
+			self.addResourceToFile(mapDataFile)
+		except FileNotFoundError:
+			print("Map info not found")
 
-			self.getter.getRelativeFile("overviews/%s.txt" % self.mapname)
-			with open("%s/maps/%s.res" % (self.archivePath, self.mapname), "a") as myfile:
-				myfile.write("\n")
-				myfile.write("overviews/%s.txt" % self.mapname)
+		## Detailed Textures File
+		mapDetailFile = "maps/%s_detail.txt" % self.mapname
+		try:
+			self.getter.getRelativeFile(mapDetailFile)
+			self.addResourceToFile(mapDetailFile)
+		except FileNotFoundError:
+			pass
+
+		## Look for overview file
+		ovrImgFile = "overviews/%s.bmp" % self.mapname
+		ovrDataFile = "overviews/%s.txt" % self.mapname
+		try:
+			self.getter.getRelativeFile(ovrImgFile)
+			self.addResourceToFile(ovrImgFile)
+			self.getter.getRelativeFile(ovrDataFile)
+			self.addResourceToFile(ovrDataFile)
 		except FileNotFoundError:
 			shutil.rmtree(os.path.join(self.archivePath, "overviews"))
 			print("Overview not found")
+
+		## Custom Objective Icons
+		# These are set on the `dod_control_point` entity via the 
+		# `point_hud_icon_<neutral|axis|allies>`property set to 
+		# values 18 through 27. ResGen doesn't detect these.
+
+		for team in ["allies", "axis", "neutral", "brit"]:
+			for index in [1, 2, 3]:
+				objfile = "sprites/obj_icons/%s/icon_obj_custom%d_%s.spr" % (self.mapname, index, team)
+				try:
+					self.getter.getRelativeFile(objfile)
+					self.addResourceToFile(objfile)
+				except FileNotFoundError:
+					pass
+
+
+	def addResourceToFile(self, relRes):
+		if relRes not in self.resources:
+			with open(self.resFile, "a") as myfile:
+				myfile.write("\n")
+				myfile.write(relRes)
+
 
 
 
@@ -95,14 +131,12 @@ class LocalGetter(ResourceGetter):
 		src = os.path.join(self.basePath, file)
 		dest = os.path.join(self.archivePath, file)
 
+		print("Get file: %s" % src)
+
 		if os.path.exists(src):
 			os.makedirs(os.path.dirname(dest), exist_ok=True)
 		
 			shutil.copyfile(src, dest)
 		else:
 			raise FileNotFoundError
-		
-		print("Get file: %s" % src)
-
-
 
